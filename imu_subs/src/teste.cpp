b0b3a5ba7def906b66ff56034a56ea95c04b30d0
williansimplicio <42345398+williansimplicio@users.noreply.github.com>
2019-06-07 12:08:13 -0300

    Debug version
    Visualize information in RQT plot

--- imu_subs/src/teste.cpp
+++ imu_subs/src/teste.cpp
@@ -3,79 +3,62 @@
 #include "std_msgs/Float64.h"
 #include "math.h"
 
-class Listener 
-{
-  public:
-    double AcX, AcY, AcZ, Roll, Pitch, Yaw, garra; 
-
-    void callback(const sensor_msgs::Imu::ConstPtr& msg);
+//Class to use values of callback function
+class Listener {
+public:
+double AcX, AcY, AcZ, Roll, Pitch, Yaw; 
+	
+void callback(const sensor_msgs::Imu::ConstPtr& msg);
 };
 
 void Listener::callback(const sensor_msgs::Imu::ConstPtr& msg) {
-  
-  AcX = msg -> linear_acceleration.x;
-  AcY = msg -> linear_acceleration.y;
-  AcZ = msg -> linear_acceleration.z;
 
-  Roll = atan2(AcY, AcZ);
-  Pitch = atan2(-AcX, sqrt(AcY*AcY+AcZ*AcZ));
-  
-	//garra = ((Pitch+1.4)/2.8)*0.03 - 0.01;
-		
-	/*Roll = msg->orientation.x;
-	Pitch = msg->orientation.y;
-	Yaw = msg->orientation.z;*/
-  
+AcX = msg -> linear_acceleration.x;
+AcY = msg -> linear_acceleration.y;
+AcZ = msg -> linear_acceleration.z;
+
+Roll = atan2(AcY, AcZ);
+Pitch = atan2(-AcX, sqrt(AcY*AcY+AcZ*AcZ));
+
 }
 
 int main(int argc, char **argv) {
-  
-  ros::init(argc, argv, "teste");
-  
-  ros::NodeHandle n;
-  ros::Rate loop_rate(100);
-	Listener Celular1, Celular2;
 
-  ros::Subscriber sub = n.subscribe("imu/data", 1000, &Listener::callback, &Celular1);
-  
-	//ros::Subscriber sub = n.subscribe("/imu/data", 1000, &Listener::callback, &Celular1);
+ros::init(argc, argv, "teste");
 
-	ros::Subscriber sub2 = n.subscribe("edTopic", 1000, &Listener::callback, &Celular2);
+ros::NodeHandle n;
+//100Hz Frequency
+ros::Rate loop_rate(100);
 
-	//Juntas
-	ros::Publisher pub4 = n.advertise<std_msgs::Float64>("/open_manipulator/joint4_position/command", 1000);
-	
-	ros::Publisher pubRoll = n.advertise<std_msgs::Float64>("roll", 1000);
-	ros::Publisher pubPitch = n.advertise<std_msgs::Float64>("pitch", 1000);
+//Objects to use values from callback
+Listener Celular1, Celular2;
 
-	//Juntas
-  std_msgs::Float64 Junta4;
+//IMU SUBSCRIBER
+ros::Subscriber sub = n.subscribe("imu/data", 1000, &Listener::callback, &Celular1);
+ros::Subscriber sub2 = n.subscribe("edTopic", 1000, &Listener::callback, &Celular2);
 
-	std_msgs::Float64 roll;
-	std_msgs::Float64 pitch;
+//DebugPublisher
+ros::Publisher pubRoll = n.advertise<std_msgs::Float64>("roll", 1000);
+ros::Publisher pubPitch = n.advertise<std_msgs::Float64>("pitch", 1000);
 
-	
-	while (ros::ok()) {
-		
-		ros::spinOnce();
+//DebugVariables
+std_msgs::Float64 roll;
+std_msgs::Float64 pitch;
 
-		//ROS_INFO("Angulo Celular1 [%.2f], Angulo Celular2 [%.2f]\n", Celular1.Pitch*180.0/M_PI, Celular2.Pitch*180.0/M_PI);
 
-		//ROS_INFO("Celular2-Celular1: [%.2f]graus\n", Celular2.Roll - Celular1.Roll);
+while (ros::ok()) {
 
-		//Junta4.data = Celular2.Pitch - Celular1.Pitch;
+	ros::spinOnce();
 
-		//pub4.publish(Junta4);
+	ROS_INFO("\nROLL: [%.2f]\nPITCH: [%.2f]\n", Celular1.Roll*180/M_PI, Celular1.Pitch*180/M_PI);
 
-		ROS_INFO("\nROLL: [%.2f]\nPITCH: [%.2f]\n", Celular1.Roll*180/M_PI, Celular1.Pitch*180/M_PI);
-		
-		roll.data = Celular1.Roll*180/M_PI;
-		pitch.data = Celular1.Pitch*180/M_PI;
-			
-		pubRoll.publish(roll);
-		pubPitch.publish(pitch);
-		loop_rate.sleep();		
-	}
-  
-  return 0;
+	roll.data = Celular1.Roll*180/M_PI;
+	pitch.data = Celular1.Pitch*180/M_PI;
+
+	pubRoll.publish(roll);
+	pubPitch.publish(pitch);
+	loop_rate.sleep();		
+}
+
+return 0;
 }
