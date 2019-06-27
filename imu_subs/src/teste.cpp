25d66025511ade00dead1d02050bde0d10e102ee
williansimplicio <42345398+williansimplicio@users.noreply.github.com>
2019-06-27 02:14:37 -0300

    Update teste.cpp
    Definition of joint angles for publishers. Use of Complementary Filter to get Yaw angle.

--- imu_subs/src/teste.cpp
+++ imu_subs/src/teste.cpp
@@ -1,64 +1,90 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
+#include "geometry_msgs/Vector3Stamped.h"
 #include "std_msgs/Float64.h"
 #include "math.h"
 
-//Class to use values of callback function
 class Listener {
-public:
-double AcX, AcY, AcZ, Roll, Pitch, Yaw; 
-	
-void callback(const sensor_msgs::Imu::ConstPtr& msg);
+	public:
+	double AcX, AcY, AcZ, Roll, Pitch, Yaw, garra; 
+
+	void callback(const sensor_msgs::Imu::ConstPtr& msg);
+	void YAWcallback(const geometry_msgs::Vector3Stamped::ConstPtr& msg);
 };
 
 void Listener::callback(const sensor_msgs::Imu::ConstPtr& msg) {
 
-AcX = msg -> linear_acceleration.x;
-AcY = msg -> linear_acceleration.y;
-AcZ = msg -> linear_acceleration.z;
+	AcX = msg -> linear_acceleration.x;
+	AcY = msg -> linear_acceleration.y;
+	AcZ = msg -> linear_acceleration.z;
 
-Roll = atan2(AcY, AcZ);
-Pitch = atan2(-AcX, sqrt(AcY*AcY+AcZ*AcZ));
+	Roll = atan2(AcY, AcZ);
+	Pitch = atan2(-AcX, sqrt(AcY*AcY+AcZ*AcZ));
+	
+	//garra = ((Pitch+1.4)/2.8)*0.03 - 0.01;
+}
 
+void Listener::YAWcallback(const geometry_msgs::Vector3Stamped::ConstPtr& msg){
+
+	Yaw = msg -> vector.z;
 }
 
 int main(int argc, char **argv) {
 
 ros::init(argc, argv, "teste");
 
+ROS_INFO("Initialing node");
 ros::NodeHandle n;
-//100Hz Frequency
-ros::Rate loop_rate(100);
+ros::Rate loop_rate(10);
+Listener Celular1, Celular2, Celular3;
 
-//Objects to use values from callback
-Listener Imu1, Imu2, Imu3;
+//ros::Subscriber sub1 = n.subscribe("Imu1", 1000, &Listener::callback, &Celular1); //Punho
 
-//IMU SUBSCRIBER
-ros::Subscriber sub1 = n.subscribe("imu/data", 1000, &Listener::callback, &Imu1);
-//ros::Subscriber sub2 = n.subscribe("imu/data", 1000, &Listener::callback, &Imu2);
-//ros::Subscriber sub3 = n.subscribe("imu/data", 1000, &Listener::callback, &Imu2);	
+//ros::Subscriber sub2 = n.subscribe("Imu2", 1000, &Listener::callback, &Celular2); //Antebraço
 
-//DebugPublisher
-ros::Publisher pubRoll = n.advertise<std_msgs::Float64>("roll", 1000);
-ros::Publisher pubPitch = n.advertise<std_msgs::Float64>("pitch", 1000);
+ros::Subscriber sub3 = n.subscribe("android/imu", 1000, &Listener::callback, &Celular3); //Ombro
 
-//DebugVariables
-std_msgs::Float64 roll;
-std_msgs::Float64 pitch;
+//ros::Subscriber subYaw = n.subscribe("/imu/rpy/filtered", 1000, &Listener::YAWcallback, &Celular3); //Yaw
+
+//Juntas
+ros::Publisher pub4 = n.advertise<std_msgs::Float64>("/open_manipulator/joint4_position/command", 1000);
+ros::Publisher pub3 = n.advertise<std_msgs::Float64>("/open_manipulator/joint3_position/command", 1000);
+ros::Publisher pub2 = n.advertise<std_msgs::Float64>("/open_manipulator/joint2_position/command", 1000);
+ros::Publisher pub1 = n.advertise<std_msgs::Float64>("/open_manipulator/joint1_position/command", 1000);
+
+
+//Juntas
+std_msgs::Float64 Junta4;
+std_msgs::Float64 Junta3;
+std_msgs::Float64 Junta2;
+std_msgs::Float64 Junta1;
 
 
 while (ros::ok()) {
 
 	ros::spinOnce();
 
-	ROS_INFO("\nROLL: [%.2f]\nPITCH: [%.2f]\n", Imu1.Roll*180/M_PI, Imu1.Pitch*180/M_PI);
+	/*Angulo Antebraço - Angulo do Punho
+	ROS_INFO("Angulo Celular1 [%.2f], Angulo Celular2 [%.2f]\n", Celular1.Roll*180.0/M_PI, Celular2.Roll*180.0/M_PI;
+	ROS_INFO("Celular2-Celular1: [%.2f]graus\n", (Celular2.Roll - Celular1.Roll)*180.0/M_PI);
+	Junta4.data = -(Celular2.Roll - Celular1.Roll); //Publicar simétrico do resultado*/
 
-	roll.data = Imu1.Roll*180/M_PI;
-	pitch.data = Imu1.Pitch*180/M_PI;
+	ROS_INFO("Roll [%.2f]\n", Celular3.Roll);
 
-	pubRoll.publish(roll);
-	pubPitch.publish(pitch);
-	loop_rate.sleep();		
+	/*Angulo Ombro - Angulo Antebraço
+	ROS_INFO("Angulo Celular2 [%.2f], Angulo Celular3 [%.2f]\n", Celular2.Roll*180.0/M_PI, Celular3.Roll*180.0/M_PI);
+	Junta3.data = -(Celular3.Roll - Celular2.Roll);
+	ROS_INFO("Celular3-Celular2: [%.2f]graus\n", (Celular3.Roll - Celular2.Roll)*180.0/M_PI);*/
+
+	/*Junta2.data = Celular3.Roll;
+	Junta1 = Celular3.Yaw
+
+	pub4.publish(Junta4);
+	pub3.publish(Junta3);
+	pub2.publish(Junta2)
+	pub1.publish(Junta1)*/
+
+	loop_rate.sleep();
 }
 
 return 0;
